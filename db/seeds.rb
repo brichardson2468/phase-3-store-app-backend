5708d86f7b896803c9f195bde10dc3c64b67cc0e
Brad Richardson <brichardson2468@gmail.com>
2022-04-19 11:48:43 -0400

    Added all REAL shoe data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,10 @@
-puts "🌱 SEEDING PANTS..."
-puts "🌱 SEEDING SHIRTS..."
-puts "🌱 SEEDING SHOES..."
-
 Pant.destroy_all
 Shirt.destroy_all
 Shoe.destroy_all
 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~PANT DATA~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
+
+puts "🌱 SEEDING PANTS..."
 
 Pant.create(
     name: "Jeans", 
@@ -123,6 +121,8 @@ Pant.create(
 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~SHIRT DATA~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
 
+puts "🌱 SEEDING SHIRTS..."
+
 Shirt.create(
     name: "Shirt", 
     image: "https://static01.nyt.com/images/2013/09/22/magazine/22wmt/22wmt-articleLarge-v2.jpg?quality=75&auto=webp&disable=upscale",
@@ -238,116 +238,118 @@ Shirt.create(
 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~SHOE DATA~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
 
+puts "🌱 SEEDING SHOES..."
+
 Shoe.create(
-    name: "Shoe", 
-    image: "https://media.cntraveler.com/photos/5f99c341a078e911295679b8/master/w_2100,h_1500,c_limit/VasqueTalus-WomensHikingBoots-2020.jpg",
-    description: "Cool shoe",
-    price: 10,
+    name: "La Sportiva", 
+    image: "https://www.rei.com/media/e40ded71-b861-4243-87c7-22e5680662ba?size=2000",
+    description: "Tarrantulace Climbing Shoes",
+    price: 85,
     bought?: false,
 )
 Shoe.create(
-    name: "Shoe", 
-    image: "https://media.cntraveler.com/photos/5f99c341a078e911295679b8/master/w_2100,h_1500,c_limit/VasqueTalus-WomensHikingBoots-2020.jpg",
-    description: "Cool shoe",
-    price: 10,
+    name: "La Sportiva", 
+    image: "https://www.rei.com/media/34c563f1-14c0-4627-8465-6d9077632094?size=2000",
+    description: "Finale Climbing Shoes",
+    price: 109,
     bought?: false,
 )
 Shoe.create(
-    name: "Shoe", 
-    image: "https://media.cntraveler.com/photos/5f99c341a078e911295679b8/master/w_2100,h_1500,c_limit/VasqueTalus-WomensHikingBoots-2020.jpg",
-    description: "Cool shoe",
-    price: 10,
+    name: "Black Diamond", 
+    image: "https://www.rei.com/media/28822d1d-f908-422f-9a4c-bc495bc77df7?size=2000",
+    description: "Momentum Climbing Shoes",
+    price: 100,
     bought?: false,
 )
 Shoe.create(
-    name: "Shoe", 
-    image: "https://media.cntraveler.com/photos/5f99c341a078e911295679b8/master/w_2100,h_1500,c_limit/VasqueTalus-WomensHikingBoots-2020.jpg",
-    description: "Cool shoe",
-    price: 10,
+    name: "La Sportiva", 
+    image: "https://www.rei.com/media/3a9d62b4-56e3-45a0-859c-cf5d4ea8ec44?size=2000",
+    description: "TC Pro Climbing Shoes",
+    price: 199,
     bought?: false,
 )
 Shoe.create(
-    name: "Shoe", 
-    image: "https://media.cntraveler.com/photos/5f99c341a078e911295679b8/master/w_2100,h_1500,c_limit/VasqueTalus-WomensHikingBoots-2020.jpg",
-    description: "Cool shoe",
-    price: 10,
+    name: "Scarpa", 
+    image: "https://www.rei.com/media/5bd7470f-744b-4679-af49-5bb9d68b7dcd?size=2000",
+    description: "Helix Climbing Shoes",
+    price: 109,
     bought?: false,
 )
 Shoe.create(
-    name: "Shoe", 
-    image: "https://media.cntraveler.com/photos/5f99c341a078e911295679b8/master/w_2100,h_1500,c_limit/VasqueTalus-WomensHikingBoots-2020.jpg",
-    description: "Cool shoe",
-    price: 10,
+    name: "La Sportiva", 
+    image: "https://www.rei.com/media/82db734f-15a5-4c03-8559-21073723e416?size=2000",
+    description: "Miura VS Climbing Shoes",
+    price: 185,
     bought?: false,
 )
 Shoe.create(
-    name: "Shoe", 
-    image: "https://media.cntraveler.com/photos/5f99c341a078e911295679b8/master/w_2100,h_1500,c_limit/VasqueTalus-WomensHikingBoots-2020.jpg",
-    description: "Cool shoe",
-    price: 10,
+    name: "La Sportiva", 
+    image: "https://www.rei.com/media/6c25ecfa-74a3-4fba-8310-9db0e5940868?size=2000",
+    description: "Kubo Climbing Shoes",
+    price: 150,
     bought?: false,
 )
 Shoe.create(
-    name: "Shoe", 
-    image: "https://media.cntraveler.com/photos/5f99c341a078e911295679b8/master/w_2100,h_1500,c_limit/VasqueTalus-WomensHikingBoots-2020.jpg",
-    description: "Cool shoe",
-    price: 10,
+    name: "Scarpa", 
+    image: "https://www.rei.com/media/3aca9f5c-68f4-4630-a250-f612a3c73382?size=2000",
+    description: "Quantic Climbing Shoes",
+    price: 169,
     bought?: false,
 )
 Shoe.create(
-    name: "Shoe", 
-    image: "https://media.cntraveler.com/photos/5f99c341a078e911295679b8/master/w_2100,h_1500,c_limit/VasqueTalus-WomensHikingBoots-2020.jpg",
-    description: "Cool shoe",
-    price: 10,
+    name: "evolv", 
+    image: "https://www.rei.com/media/0a3400a7-5f67-470a-8529-be5305ac862c?size=2000",
+    description: "Kronos Climbing Shoes",
+    price: 130,
     bought?: false,
 )
 Shoe.create(
-    name: "Shoe", 
-    image: "https://media.cntraveler.com/photos/5f99c341a078e911295679b8/master/w_2100,h_1500,c_limit/VasqueTalus-WomensHikingBoots-2020.jpg",
-    description: "Cool shoe",
-    price: 10,
+    name: "Five Ten", 
+    image: "https://www.rei.com/media/8236f938-ac33-4764-bce7-fe67785a9db9?size=2000",
+    description: "NIAD Moccasym Climbing Shoes",
+    price: 125,
     bought?: false,
 )
 Shoe.create(
-    name: "Shoe", 
-    image: "https://media.cntraveler.com/photos/5f99c341a078e911295679b8/master/w_2100,h_1500,c_limit/VasqueTalus-WomensHikingBoots-2020.jpg",
-    description: "Cool shoe",
-    price: 10,
+    name: "La Sportiva", 
+    image: "https://www.rei.com/media/91b8f7d6-d459-4272-a357-adc8cfead976?size=2000",
+    description: "Skwama Climbing Shoes",
+    price: 180,
     bought?: false,
 )
 Shoe.create(
-    name: "Shoe", 
-    image: "https://media.cntraveler.com/photos/5f99c341a078e911295679b8/master/w_2100,h_1500,c_limit/VasqueTalus-WomensHikingBoots-2020.jpg",
-    description: "Cool shoe",
-    price: 10,
+    name: "evolv", 
+    image: "https://www.rei.com/media/f14ba0e1-9663-4e7a-a87f-c3dc1c4b740a?size=2000",
+    description: "Geshido Climbing Shoes",
+    price: 160,
     bought?: false,
 )
 Shoe.create(
-    name: "Shoe", 
-    image: "https://media.cntraveler.com/photos/5f99c341a078e911295679b8/master/w_2100,h_1500,c_limit/VasqueTalus-WomensHikingBoots-2020.jpg",
-    description: "Cool shoe",
-    price: 10,
+    name: "Black Diamond", 
+    image: "https://www.rei.com/media/33747c21-cf95-4568-ab1a-f81be9b19b2b?size=2000",
+    description: "Zone LV Climbing Shoes",
+    price: 140,
     bought?: false,
 )
 Shoe.create(
-    name: "Shoe", 
-    image: "https://media.cntraveler.com/photos/5f99c341a078e911295679b8/master/w_2100,h_1500,c_limit/VasqueTalus-WomensHikingBoots-2020.jpg",
-    description: "Cool shoe",
-    price: 10,
+    name: "TENAYA", 
+    image: "https://www.rei.com/media/9ed47f61-7e9c-4324-bccf-7be5778107ca?size=2000",
+    description: "Mundaka Climbing Shoes",
+    price: 195,
     bought?: false,
 )
 Shoe.create(
-    name: "Shoe", 
-    image: "https://media.cntraveler.com/photos/5f99c341a078e911295679b8/master/w_2100,h_1500,c_limit/VasqueTalus-WomensHikingBoots-2020.jpg",
-    description: "Cool shoe",
-    price: 10,
+    name: "Butora", 
+    image: "https://www.rei.com/media/13b8d7e0-1926-4779-8ec0-47426e6b05e6?size=2000",
+    description: "Gomi (Narrow Fit) Climbing Shoes",
+    price: 164,
     bought?: false,
 )
 Shoe.create(
-    name: "Shoe", 
-    image: "https://media.cntraveler.com/photos/5f99c341a078e911295679b8/master/w_2100,h_1500,c_limit/VasqueTalus-WomensHikingBoots-2020.jpg",
-    description: "Cool shoe",
-    price: 10,
+    name: "So iLL", 
+    image: "https://www.rei.com/media/92eace68-b74d-4b05-857d-057bcddb2292?size=2000",
+    description: "Street LV Climbing Shoes",
+    price: 139,
     bought?: false,
 )
     
