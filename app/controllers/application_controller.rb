9dbfa14a2c73f83caf30c97ccfa8ee59e85fe40f
wesley <wesbourkeavl@gmail.com>
2022-04-19 16:31:04 -0400

    post methods for shirt, pants and shoes. also added bought counter increase in the same function.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,7 +17,7 @@ class ApplicationController < Sinatra::Base
     Purchase.all.to_json
   end  
 
-  post "/purchases" do
+  post "/shoe-purchases" do
     purchases = Purchase.create(
       pant_id: params[:pant_id],
       shirt_id: params[:shirt_id],
@@ -26,6 +26,30 @@ class ApplicationController < Sinatra::Base
     a = Shoe.find_by(id: params[:shoe_id]).bought
     a = a + 1
     Shoe.find_by(id: params[:shoe_id]).update(bought: a)
+    Purchase.all.to_json
+  end
+
+  post "/pant-purchases" do
+    purchases = Purchase.create(
+      pant_id: params[:pant_id],
+      shirt_id: params[:shirt_id],
+      shoe_id: params[:shoe_id],
+    )
+    a = Pant.find_by(id: params[:pant_id]).bought
+    a = a + 1
+    Pant.find_by(id: params[:pant_id]).update(bought: a)
+    Purchase.all.to_json
+  end
+
+  post "/shirt-purchases" do
+    purchases = Purchase.create(
+      pant_id: params[:pant_id],
+      shirt_id: params[:shirt_id],
+      shoe_id: params[:shoe_id],
+    )
+    a = Shirt.find_by(id: params[:shirt_id]).bought
+    a = a + 1
+    Shirt.find_by(id: params[:shirt_id]).update(bought: a)
     Purchase.all.to_json
   end
 
